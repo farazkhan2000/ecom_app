3e5a2a6722e82c67792f9c52705c4e8a7b25eac9
farazkhan2000 <f.skipper14@gmail.com>
2024-11-17 12:42:15 +0500

    NewsletterBox component added

--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -10,12 +10,12 @@ const OurPolicy = () => {
         <p className='text-gray-400'>We offer hassle free exchange policy</p>
       </div>
       <div>
-        <img className='w-12 m-auto mb-5' src={assets.exchange_icon} alt="" />
+        <img className='w-12 m-auto mb-5' src={assets.quality_icon} alt="" />
         <p className='font-semibold'>7 Days Return Policy</p>
         <p className='text-gray-400'>We provide 7 days free return policy</p>
       </div>
       <div>
-        <img className='w-12 m-auto mb-5' src={assets.exchange_icon} alt="" />
+        <img className='w-12 m-auto mb-5' src={assets.support_img} alt="" />
         <p className='font-semibold'>Best customer support</p>
         <p className='text-gray-400'>We provide 24/7 customer support</p>
       </div>
